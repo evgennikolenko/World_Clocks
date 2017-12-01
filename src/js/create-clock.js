bb91dd5501fe03f6e0eae20dbc8873782f490020
Evgen Nikolenko <evgennikolenko@ukr.net>
2017-12-01 23:41:12 +0200

    #2

--- src/js/create-clock.js
+++ src/js/create-clock.js
@@ -2,11 +2,11 @@
  * Created by evgennikolenko on 26.11.17.
  */
 
-var CreateClock = function (classNameCountry, timeZone, gmtName) {
-
+var CreateClock = function (classNameCountry, timeZone, gmtName, date) {
     this.classNameCountry = classNameCountry; // добавление дополнительного класса с названием города
     this.timeZone = +timeZone; // числовое значение часового пояса
     this.gmtName = gmtName; // GMT
+    this.date = date; // GMT
 };
 
 /*
@@ -32,65 +32,35 @@ CreateClock.prototype.getTimeWithZone = function () {
     var getTime = strArrayDate[0].split(":"), //время hh:mm:ss
      getHoursAndMinute = getTime[0] +':'+ getTime[1]; // hh:mm
 
-    if(getHoursAndMinute === '00:00'){
-        day += 1;
-    }
-
     var getSeconds = getTime[2]; // ss
     var getGMT = "GMT+0"+this.timeZone+"00"; //пояс
 
     return {
         'time' : getHoursAndMinute,
         'seconds' : getSeconds,
-        'timeZone': getGMT,
-
-        'day' : day,
-        'month' : month,
-        'year' : year
+        'timeZone': getGMT
     }
 };
 
 /*
 * createDom строет DOM дерево с созданым обьектом-часами
-* Строение:
-* div .clock +(classNameCountry)
-*       div .clock__time  ---> time
-*       div .time__seconds  ---> seconds
-*       div .clock__zone-name  ---> zone name ```(Europe/London)
-*       div .clock__memo ---> some text
 */
 CreateClock.prototype.createDom = function () {
-    function getMonthName(month) {
-        var arr = ["January", "February", "March", "April", "May",
-            "June" , "July", "August", "September", "October", "November", "December"];
-        for(var i = 0; i < arr.length; i++){
-            if(month === i) {
-                return arr[i];
-            }
-        }
-    }
-
-    var day = this.getTimeWithZone().day;
-    var month = getMonthName(this.getTimeWithZone().month);
-    var year = this.getTimeWithZone().year;
-    // Current time in <span class="header__country"> Kiev</span>
     var clock = '<div class="clock '+  this.classNameCountry+'">'+
-       ' <div class="clock__header"></div>'+
+        '<div class="clock__header"></div>'+
         '<div class="clock__time ">'+this.getTimeWithZone().time+'' +
-        '</div>' + '<span class="time__seconds">'+this.getTimeWithZone().seconds+'</span>'+
-      '  <div class="clock__get-date">'+ day + " " + month + " " + year+'</div>'+
-        '  <div class="clock__weather">'+
-    ' <div class="weather__temp">6 C</div>'+
-    '<img class="weather__image">'+
-    '<div class="weather__wind">17.5</div>'+
-
-    ' <div class="weather__sun">'+
-    ' <div class="sunrise">Sunrise: <span class="sunrise__div">12:35</span></div>'+
-    ' <div class="sunset">Sunset:  <span class="sunset__div">24:00</span></div>'+
-    ' </div>'+
-    ' </div>'+
+        '</div>'+'<span class="time__seconds">'+this.getTimeWithZone().seconds+'</span>'+
+        '<div class="clock__get-date">'+ this.date +'</div>'+
+        '<div class="clock__weather">'+
+        '<div class="weather__temp">6 C</div>'+
+        '<div class="weather__image"></div>'+
+        '<div class="weather__wind"><span class="wind-speed__val"></span><div class="wind__image"></div></div>'+
+        '<div class="weather__sun">'+
+        '<div class="sunrise">Sunrise: <span class="sunrise__div">12:35</span></div>'+
+        '<div class="sunset">Sunset:  <span class="sunset__div">24:00</span></div>'+
+        '</div>'+
+        '</div>'+
         '<div class="clock__zone-name">'+this.gmtName+'</div>';
-
 
     $(".clock__container").append(clock);
 };
--- src/js/main.js
+++ src/js/main.js
@@ -2,42 +2,33 @@
  * Created by evgen on 27.11.17.
  */
 window.addEventListener('load', function () {
-    var DATAjson;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "data.json", true);
-    xhr.addEventListener('load', function () {
-        if (xhr.status < 400) {
-            var dataJSON = JSON.parse(xhr.responseText);
-            console.log("dara ", dataJSON);
-            DATAjson = dataJSON["data"];
-            // MORZA = alphabetJSON["morza"];
-        }
-    });
-    xhr.send(null);
 
+        var DATAjson;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "data.json", true);
+        xhr.addEventListener('load', function () {
+            if (xhr.status < 400) {
+                var dataJSON = JSON.parse(xhr.responseText);
+                DATAjson = dataJSON["data"];
+            }
+        });
+        xhr.send(null);
 
     // forms elements
     var formButton = document.querySelector('.create-form__button');
     var formOptions = document.querySelectorAll('.select__options');
     console.log(formOptions);
 
-
     formButton.addEventListener('click', function (e) {
-
 
         // select ---> options
         for ( var i = 0; i < formOptions.length; i++){
             var option = formOptions[i];
-
-            console.log("option: ", formOptions[i]);
-
             if ( option.selected){
                 var getDataAttr = option.getAttribute('data-city-name');
-                console.log(getDataAttr);
                 break;
             }
         }
-
                 for ( i in DATAjson){
                     if(i === getDataAttr){
                         var key = DATAjson[i];
@@ -49,11 +40,8 @@ window.addEventListener('load', function () {
                     }
                 }
 
-
-        console.log("re ",DATAjson);
-
-
-        var searchtext = "select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where text='" + getDataAttr + "') and u='c'";
+        var searchtext = "select%20*%20from%20weather.forecast%20where%20woeid%20in%20" +
+            "(select%20woeid%20from%20geo.places(1)%20where text='" + getDataAttr + "') and u='c'";
         var src = "https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json";
 
         getYahooAjax(src, function (data) {
@@ -62,7 +50,6 @@ window.addEventListener('load', function () {
             console.log("jahoo",jahoo);
 
             var weather = jahoo.item.condition;
-            console.log("weather", weather);
 
             var temperature = weather.temp,
                 text = weather.text;
@@ -70,45 +57,34 @@ window.addEventListener('load', function () {
             var sunrise = jahoo.astronomy.sunrise,
                 sunset = jahoo.astronomy.sunset;
 
-            $("."+ className +" .weather__temp").text(temperature);
-            $("."+ className +" .weather__wind").text(wind);
+            var date = jahoo.lastBuildDate;
+            date = date.split(" ").slice(1,4).join(" ");
+            
+            // создание екземпляра
+            var createClock = new CreateClock( className, timeZone, gmtName, date);
+            createClock.createDom();
+
+            $("."+ className +" .clock__header").text('Current time in '+ getDataAttr  );
+
+            $("."+ className +" .weather__temp").text(temperature + ' °C');
+            $("."+ className +" .wind-speed__val").text(wind);
             $("."+ className +" .sunrise__div").text(sunrise);
             $("."+ className +" .sunset__div").text(sunset);
+            //
+            $("."+className). css("background", backgroundImg );
+            $("."+className). css("background-size", "cover" );
+
+            setInterval(function () {
+                var dateInWorld = createClock.getTimeWithZone();
+                $("."+ className +" .clock__time").text(dateInWorld.time);
+                $("."+ className +" .time__seconds").text(dateInWorld.seconds);
+            }, 1000);
         });
-
-
-
-
-        // создание екземпляра
-        var createClock = new CreateClock( className, timeZone, gmtName);
-        createClock.createDom();
-        $("."+ className +" .clock__header").text('Current time in '+ getDataAttr  );
-        //
-        $("."+className). css("background", backgroundImg );
-        $("."+className). css("background-size", "cover" );
-        // $(".sun_up").text(weatherYahoo().sunrise);
-
-
-
-
-        // '"' + backgroundImg + '"'
-
-
-        setInterval(function () {
-            var dateInWorld = createClock.getTimeWithZone();
-
-            $("."+ className +" .clock__time").text(dateInWorld.time);
-
-            $("."+ className +" .time__seconds").text(dateInWorld.seconds);
-        }, 1000);
         e.preventDefault();
     });
 });
 
-
-
 function getYahooAjax(url, callback) {
-
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -124,6 +100,7 @@ function getYahooAjax(url, callback) {
     xhr.open("GET", url, true);
     xhr.send(null);
 }
+
 
 
 
